2f228f5319749e5053d4193eb230c61a9e68081c
rkn-code <rikano.yamauchi@gmail.com>
2023-08-22 19:35:30 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const CursorFx = window['CursorFx'].CursorFx;
+
+
+// Vue.component(
+//   CursorFx.name,
+//   CursorFx
+// );
+// new Vue({
+//   el: '#app'
+// });
+  
+// Vue.createApp({
+//     data(){
+//         return {
+//            render Vue({el: '#app'});
+//         }
+//     }
+// }).mount('#app')
